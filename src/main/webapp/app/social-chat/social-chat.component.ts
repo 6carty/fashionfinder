773341db269e7e91ecad44f9658bf46c23f063a1
Tameem <txc180@student.bham.ac.uk>
2024-03-05 23:04:49 +0000

    Social Chat - Added examples messages, sent messaged need to be positioned correctly

--- src/main/webapp/app/social-chat/social-chat.component.ts
+++ src/main/webapp/app/social-chat/social-chat.component.ts
@@ -13,6 +13,20 @@ export class SocialChatComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   private accountSubscription: Subscription | null = null;
 
+  messages = [
+    { text: 'Hey, how are you', isSent: true, time: '12:50' },
+    { text: 'I am good hbu', isSent: false, time: '13:00' },
+    { text: 'Yeah I am doing good too', isSent: true, time: '13:05' },
+    { text: 'I am so excited to post my outfit today', isSent: true, time: '13:00' },
+    { text: 'Must be good', isSent: false, time: '13:01' },
+    { text: "You're gonna love it just posted it", isSent: true, time: '13:10' },
+    { text: 'Wow you were right I love your outfit today', isSent: false, time: '13:00' },
+    { text: "Thanks Bob, can't wait to see what you're wearing today", isSent: false, time: '13:01' },
+    { text: 'Just posted it', isSent: true, time: '13:10' },
+    { text: 'What you think?', isSent: true, time: '13:11' },
+    { text: 'That looks sick 🔥🔥🔥', isSent: false, time: '13:15' },
+  ];
+
   constructor(private accountService: AccountService, private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
