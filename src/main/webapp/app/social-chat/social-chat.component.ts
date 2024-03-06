cdc7746c86be5263c2ae9adb66b704559fb43550
Tameem <txc180@student.bham.ac.uk>
2024-03-06 07:37:47 +0000

    Social Chat - Added frontend of list of chatrooms and chat header

--- src/main/webapp/app/social-chat/social-chat.component.ts
+++ src/main/webapp/app/social-chat/social-chat.component.ts
@@ -27,6 +27,24 @@ export class SocialChatComponent implements OnInit, OnDestroy {
     { text: 'That looks sick 🔥🔥🔥', isSent: false, time: '13:15' },
   ];
 
+  chatrooms = [
+    { name: 'Sukhraj Mann', lastMessage: 'That looks sick 🔥🔥🔥', image: 'content/images/jhipster_family_member_1_head-192.png' },
+    { name: 'Harry Richards', lastMessage: 'Thanks for the hoodie you...', image: 'content/images/jhipster_family_member_2_head-192.png' },
+    { name: 'Bob John', lastMessage: 'Nice', image: 'content/images/jhipster_family_member_3_head-192.png' },
+    { name: 'Pravjot Samra', lastMessage: 'Did you see the...', image: 'content/images/jhipster_family_member_2_head-192.png' },
+    { name: 'Steve Jobs', lastMessage: 'This app is amazing', image: 'content/images/jhipster_family_member_3_head-192.png' },
+    { name: 'Darwin Nunez', lastMessage: 'I need new joggers...', image: 'content/images/jhipster_family_member_2_head-192.png' },
+    { name: 'Hasaan Ahmed', lastMessage: 'Thanks', image: 'content/images/jhipster_family_member_3_head-192.png' },
+    { name: 'Michael Joe', lastMessage: 'Where did you buy...', image: 'content/images/jhipster_family_member_2_head-192.png' },
+  ];
+  selectedChatroom = null;
+
+  currentChatPartner = {
+    name: 'Sukhraj Mann',
+    lastSeen: new Date(), // Replace with actual last seen data
+    image: 'content/images/jhipster_family_member_1_head-192.png', // Replace with actual image path
+  };
+
   constructor(private accountService: AccountService, private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
@@ -34,7 +52,23 @@ export class SocialChatComponent implements OnInit, OnDestroy {
       this.account = account;
       // Trigger change detection manually
       this.changeDetectorRef.detectChanges();
+
+      // Load chat partner data when intializing component
+      // this.loadCurrentChatPartner();
+
+      // Load current selected chatroom
+      // this.selectedChatroom();
     });
+
+    // selectChatroom(chatroom): void {
+    //   this.selectedChatroom = chatroom;
+    //   // Load the messages for selected chatroom
+    //   // You can implement loading messages from backend here
+    // }
+
+    // loadCurrentChatPartner(): void {
+    //   Load the current chat partner details here
+    // }
   }
 
   ngOnDestroy(): void {
