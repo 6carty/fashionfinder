e81d0926c66febfcc56a32b49248ac5a847feb99
James <james@waller-family.com>
2024-03-07 16:54:41 +0000

    added button to add clothingItem

--- src/main/webapp/app/wardrobe/wardrobe.component.ts
+++ src/main/webapp/app/wardrobe/wardrobe.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 //import { ClothingItemService } from '../entities/clothing-item/service/clothing-item.service';
-import { EntityArrayResponseType, ClothingItemService } from '../entities/clothing-item/service/clothing-item.service';
+import { ClothingItemService } from '../entities/clothing-item/service/clothing-item.service';
+import { NewClothingItem } from '../entities/clothing-item/clothing-item.model';
+import { Status } from '../entities/enumerations/status.model';
+import { ClothingType } from '../entities/enumerations/clothing-type.model';
 
 @Component({
   selector: 'jhi-wardrobe',
@@ -10,6 +13,15 @@ import { EntityArrayResponseType, ClothingItemService } from '../entities/clothi
 export class WardrobeComponent implements OnInit {
   recievedData: any;
   ownerId: any;
+  currentUser: any;
+  highestId: any;
+  tempvalue: any;
+
+  formData = {
+    name: '',
+    occasion: '',
+    colour: '',
+  };
   constructor(private clothingItemService: ClothingItemService) {}
 
   ngOnInit(): void {
@@ -26,14 +38,31 @@ export class WardrobeComponent implements OnInit {
       this.recievedData = clothingItems.body;
     });
 
-    //   this.clothingItemService.query(request).subscribe(clothingItems => {
-    //     this.recievedData = clothingItems.body;
-    //     console.log(this.recievedData);
-    //     this.recievedData.forEach((clothingItem: { owner: { id: any; }; }) =>{
-    //       this.ownerId= clothingItem.owner.id;
-    //       console.log(this.ownerId);
-    //     });
+    this.recievedData.unsubscribe();
+  }
 
-    //    });
+  onSubmit(): void {
+    this.clothingItemService.query('include.owner').subscribe(clothingItems => {
+      this.recievedData = clothingItems.body;
+    });
+
+    this.recievedData.unsubscribe();
+
+    this.highestId = 0;
+    for (this.tempvalue in this.recievedData) {
+      if (this.tempvalue.id >= this.highestId) {
+        this.highestId = this.tempvalue.id + 1;
+      }
+      const newClothingItem: NewClothingItem = {
+        id: this.highestId,
+        name: this.formData.name,
+        style: this.formData.occasion,
+        colour: this.formData.colour,
+        status: Status.NOTFORSALE,
+        type: ClothingType.OTHERS,
+      };
+
+      this.clothingItemService.create(newClothingItem);
+    }
   }
 }
