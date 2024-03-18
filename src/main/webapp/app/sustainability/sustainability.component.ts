cd71c4ad3925b02e65442357be3de67e85c16b13
Timothy <sty138@student.bhamac.uk>
2024-03-18 12:30:54 +0000

    Revert "Made changes to the banner and retrieve data from backend to display on frontend for exchange request"
    This reverts commit b9228d1409bbe1511d410319f1eff96db0600ac9.

--- src/main/webapp/app/sustainability/sustainability.component.ts
+++ src/main/webapp/app/sustainability/sustainability.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ExchangeRequestService} from "../entities/exchange-request/service/exchange-request.service";
-import { IExchangeRequest} from "../entities/exchange-request/exchange-request.model";
 
 @Component({
   selector: 'jhi-sustainability',
@@ -8,24 +6,7 @@ import { IExchangeRequest} from "../entities/exchange-request/exchange-request.m
   styleUrls: ['./sustainability.component.scss'],
 })
 export class SustainabilityComponent implements OnInit {
-  exchangeRequest: IExchangeRequest | null = null;
+  constructor() {}
 
-  constructor(private exchangeRequestService: ExchangeRequestService) {}
-
-  ngOnInit(): void {
-    // Fetch exchange request data
-    this.loadExchangeRequestDetails();
-  }
-  loadExchangeRequestDetails(): void {
-    // Assuming you have a method in your service to fetch exchange request details by ID
-    const exchangeRequestId = 1; // Replace this with the actual ID
-    this.exchangeRequestService.find(exchangeRequestId).subscribe(
-      (res) => {
-        this.exchangeRequest = res.body;
-      },
-      (error) => {
-        console.error('Error fetching exchange request details:', error);
-      }
-    );
-  }
+  ngOnInit(): void {}
 }
