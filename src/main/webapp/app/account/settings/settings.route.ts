3baa50d55260a12efa2a2821cfd225c32bbe7e6d
Haydon Carty <sixcarty@HCs-MacBook-Pro.local>
2024-03-24 18:03:37 +0000

    Changing the route link for the Settings button

--- src/main/webapp/app/account/settings/settings.route.ts
+++ src/main/webapp/app/account/settings/settings.route.ts
@@ -2,6 +2,8 @@ import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
 import { SettingsComponent } from './settings.component';
+import { UserProfileComponent } from '../../entities/user-profile/list/user-profile.component';
+import { PostComponent } from '../../entities/post/list/post.component';
 
 export const settingsRoute: Route = {
   path: 'settings',
