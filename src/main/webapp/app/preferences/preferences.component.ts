4daae16d0a72905820a1a431246885e848a0b7bc
James <james@waller-family.com>
2024-04-17 23:56:50 +0100

    the font size for the website is now changeable is the preferences tab

--- src/main/webapp/app/preferences/preferences.component.ts
+++ src/main/webapp/app/preferences/preferences.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DarkModeService } from 'app/shared/dark-mode.service';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'jhi-preferences',
@@ -7,10 +8,41 @@ import { DarkModeService } from 'app/shared/dark-mode.service';
   styleUrls: ['./preferences.component.scss'],
 })
 export class PreferencesComponent implements OnInit {
-  constructor(private darkModeService: DarkModeService) {}
+  constructor(private darkModeService: DarkModeService, private renderer: Renderer2) {}
+
+  value: number = 1;
 
   toggleDarkMode(): void {
     this.darkModeService.toggleDarkMode();
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (localStorage.getItem('--font-base-size') == null) {
+      localStorage.setItem('--font-base-size', this.value.toString());
+    } else {
+      var tempvalue = localStorage.getItem('--font-base-size');
+      if (tempvalue) this.value = +tempvalue;
+    }
+  }
+
+  updateFontBaseSize(newSize: number) {
+    document.documentElement.style.setProperty('--font-base-size', newSize.toString());
+  }
+
+  increaseFontSize() {
+    var value = localStorage.getItem('--font-base-size');
+
+    if (value && +value < 1.3) {
+      this.updateFontBaseSize(+value + 0.1);
+      localStorage.setItem('--font-base-size', (+value + 0.1).toString());
+    }
+  }
+
+  decreaseFontSize() {
+    var value = localStorage.getItem('--font-base-size');
+
+    if (value && +value > 0.7) {
+      this.updateFontBaseSize(+value - 0.1);
+      localStorage.setItem('--font-base-size', (+value - 0.1).toString());
+    }
+  }
 }
