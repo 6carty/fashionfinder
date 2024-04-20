9ee5241d0a94c569e104a22c56d71e73f8342b80
ThereThereHere <jake.pemba1@gmail.com>
2024-04-20 08:20:58 +0100

    diary now returns dummy values

--- src/main/webapp/app/diary/diary.component.ts
+++ src/main/webapp/app/diary/diary.component.ts
@@ -3,9 +3,13 @@ import { ItemLogService } from '../entities/item-log/service/item-log.service';
 import { EntityArrayResponseType } from '../entities/event/service/event.service';
 import { IItemLog } from '../entities/item-log/item-log.model';
 import { Account } from '../core/auth/account.model';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AccountService } from '../core/auth/account.service';
+import { IUser } from '../entities/user/user.model';
+import { UserService } from '../entities/user/user.service';
+import { UserProfileService } from '../entities/user-profile/service/user-profile.service';
+import { IUserProfile } from '../entities/user-profile/user-profile.model';
 
 @Component({
   selector: 'jhi-diary',
@@ -16,19 +20,71 @@ export class DiaryComponent implements OnInit {
   protected itemLogs: any | IItemLog[] = [];
   protected account: Account | null = null;
   private accountSubscription: Subscription | null = null;
-  constructor(private router: Router, private accountService: AccountService, private itemLogService: ItemLogService) {}
+  protected logReceivedData: IItemLog[] | null = null;
+  constructor(
+    private router: Router,
+    private accountService: AccountService,
+    private itemLogService: ItemLogService,
+    private userService: UserService,
+    private userProfileService: UserProfileService,
+    protected activatedRoute: ActivatedRoute
+  ) {}
+  givenId: number = -1;
+  active: Account | undefined = undefined;
+  users: IUser[] | null = null;
+  user: IUser | undefined = undefined;
+  userProfiles: IUserProfile[] | undefined = undefined;
+  userProfile: IUserProfile | undefined = undefined;
+  userProfilePick: Pick<IUserProfile, 'id'> | null = null;
+  itemLogToEdit: IItemLog | null = null;
+  id: number = 0;
 
   ngOnInit(): void {
     this.accountSubscription = this.accountService.identity().subscribe((account: Account | null) => {
       this.account = account;
     });
-    this.fetchItemLogs();
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.givenId = params.id;
+
+      this.accountService.identity().subscribe(account => {
+        if (account) this.active = account;
+
+        this.userService.query().subscribe(users => {
+          this.users = users.body;
+          if (this.users) this.user = this.users.find(user => user.login === this.active?.login);
+          if (this.user) {
+            const pickUser: Pick<IUser, 'id'> = this.user;
+            const queryObject = {
+              'user.equal': pickUser,
+            };
+            this.userProfileService.query(queryObject).subscribe(userProfile => {
+              if (userProfile.body) {
+                this.userProfiles = userProfile.body.filter(obj => obj.user?.id == this.user?.id);
+                this.userProfile = this.userProfiles[0];
+                this.userProfilePick = this.userProfile;
+              }
+
+              this.fetchItemLogs();
+            });
+          }
+        });
+      });
+    });
   }
 
   fetchItemLogs() {
-    this.itemLogService.query('include.owner').subscribe((itemLogs: EntityArrayResponseType) => {
-      this.itemLogs = itemLogs.body || [];
-      //this.filterLogs();
+    this.itemLogService.query().subscribe(itemLogs => {
+      this.logReceivedData = itemLogs.body;
+      if (this.logReceivedData) this.logReceivedData = this.logReceivedData.filter(obj => obj.owner?.id == this.userProfile?.id);
+
+      if (this.logReceivedData) {
+        for (let item of this.logReceivedData) {
+          if (item.id == this.givenId) {
+            this.itemLogToEdit = item;
+            this.id = this.itemLogToEdit.id;
+          }
+        }
+      }
     });
   }
 
