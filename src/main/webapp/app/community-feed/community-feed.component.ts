979348c60f147c88dff88c183016128f41e2cc6f
Haydon Carty <sixcarty@HCs-MacBook-Pro.local>
2024-04-20 19:27:45 +0100

    Sorting Posts and Ensuring Captions and Comments match correct post

--- src/main/webapp/app/community-feed/community-feed.component.ts
+++ src/main/webapp/app/community-feed/community-feed.component.ts
@@ -11,9 +11,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserProfileService } from '../entities/user-profile/service/user-profile.service';
 import { UserManagementService } from '../admin/user-management/service/user-management.service';
 import { AccountService } from '../core/auth/account.service';
-import { IUser } from '../entities/user/user.model';
 import { UserService } from '../entities/user/user.service';
-import { end } from '@popperjs/core';
+import { IComment } from '../entities/comment/comment.model';
+import { CommentService } from '../entities/comment/service/comment.service';
 
 @Component({
   selector: 'jhi-community-feed',
@@ -22,29 +22,33 @@ import { end } from '@popperjs/core';
 })
 export class CommunityFeedComponent implements OnInit {
   allPosts: Observable<IPost[]> | null = null;
-  userSub: Observable<IUser[]> | null = null;
-  allUsers: IUser[] | null = null;
-  filteredUsers: IUser[] | null = null;
+  feedPosts: IPost[] | null = null;
+  feedUsernames: { username: string; imageType: any; imageURL: any }[][] = [];
+  tempUsername: any;
+  feedUserID: any;
 
   userProfileSub: Observable<IUserProfile[]> | null = null;
-  allUserProfiles: IUserProfile[] | null = null;
   filteredProfile: IUserProfile[] | null = null;
-  feedUserProfile: IUserProfile[] | undefined;
+
   currentID: any;
 
   account: Account | null = null;
   accountSubscription: Subscription | null = null;
 
-  feedUsername: any;
-  feedUserLocation: string | null | undefined;
-  feedUserID: any;
-  feedPosts: IPost[] | null = null;
-  feedUser: IUser[] | null = null;
+  allComments: Observable<IComment[]> | null = null;
+  feedComments: IComment[] | null = null;
+  visibleComment: any;
+  commentFrom: any;
 
-  someID: any;
+  quickID: any;
+  currentCommentPos: number = 0;
+  userPos: number = 0;
+
+  everyPostsInfo: { visibleComment: string; commentFrom: string; postID: any }[][] = [];
 
   constructor(
     protected postService: PostService,
+    protected commentService: CommentService,
     protected activatedRoute: ActivatedRoute,
     public router: Router,
     protected sortService: SortService,
@@ -73,18 +77,83 @@ export class CommunityFeedComponent implements OnInit {
     }
   }
 
-  getPostAuthor(postAuthor: IPost): void {
+  filterPosts(): void {
+    this.allPosts = null;
+    this.feedPosts = null;
+
+    this.allPosts = this.postService.getPosts();
+
+    this.allPosts.subscribe(currentUsersPosts => {
+      this.feedPosts = currentUsersPosts;
+      this.feedPosts = currentUsersPosts.filter(post => post.author?.id !== this.currentID);
+      /*this.feedPosts.sort((a, b) => {
+        if (a.createdDate && b.createdDate) {
+          return b.createdDate.date() - a.createdDate.date();
+        }
+        // Handle the case where postCommented is undefined
+        return 0;
+      });*/
+
+      if (this.feedPosts) {
+        this.feedPosts.forEach((post, index) => {
+          this.feedUsernames.push([{ username: '', imageType: '', imageURL: '' }]);
+        });
+      }
+      for (let i = 0; i < this.feedPosts.length; i++) {
+        this.getPostAuthor(this.feedPosts[i], i);
+        this.filterComments(this.feedPosts[i].id, i);
+      }
+    });
+  }
+
+  filterComments(postID: number, index: number): void {
+    this.everyPostsInfo.push([{ visibleComment: '', commentFrom: '', postID: '' }]);
+    this.allComments = this.commentService.getComments();
+
+    this.allComments.subscribe(currentUsersComments => {
+      //get all comments for a specific post
+      this.feedComments = currentUsersComments;
+      this.feedComments.sort((a, b) => {
+        if (a.postCommented && b.postCommented) {
+          return a.postCommented.id - b.postCommented.id;
+        }
+        // Handle the case where postCommented is undefined
+        return 0;
+      });
+      this.feedComments = currentUsersComments.filter(comment => comment.postCommented?.id === postID);
+      //console.log(this.feedComments , "filtered" , postID)
+      this.visibleComment = '';
+      if (this.feedComments.length) {
+        this.visibleComment = this.feedComments[0].content;
+        this.quickID = this.feedComments[0].userCommented?.id;
+        this.everyPostsInfo[this.currentCommentPos][0].visibleComment = this.visibleComment;
+        this.everyPostsInfo[this.currentCommentPos][0].postID = postID;
+      }
+      this.getCommentAuthor(this.quickID, this.currentCommentPos);
+      this.currentCommentPos += 1;
+    });
+  }
+
+  getCommentAuthor(postAuthorID: number | undefined, index: number): void {
+    this.commentFrom = '';
     if (this.userProfileSub) {
-      console.log('over here pls first');
       this.userProfileSub.subscribe(userProfiles => {
-        const userProfile = userProfiles.find(profile => profile.id == postAuthor.author?.id); //postAuthor.author?.id
+        let userProfile = userProfiles.find(profile => profile.id === postAuthorID);
         if (userProfile?.id) {
-          this.feedUserID = userProfile.user?.id;
           // Now that we have the user profile, get the username
           this.userService.getUsers().subscribe(users => {
-            const user = users.find(user => user.id == this.feedUserID);
-            if (user?.id) {
-              this.feedUsername = user.login;
+            let user = users.find(user => user.id === userProfile?.user?.id);
+            if (user?.login) {
+              this.commentFrom = user.login;
+              this.everyPostsInfo[index][0].commentFrom = this.commentFrom;
+              this.userPos += 1;
+              if (this.userPos === this.everyPostsInfo.length) {
+                console.log('before sort');
+                console.log(this.everyPostsInfo);
+                this.everyPostsInfo.sort((a, b) => a[0].postID - b[0].postID);
+                console.log('after sort');
+                console.log(this.everyPostsInfo);
+              }
             }
           });
         }
@@ -92,20 +161,27 @@ export class CommunityFeedComponent implements OnInit {
     }
   }
 
-  filterPosts(): void {
-    this.allPosts = this.postService.getPosts();
-    this.allPosts.subscribe(currentUsersPosts => {
-      this.feedPosts = currentUsersPosts.filter(post => post.author?.id !== this.currentID);
-      if (this.feedPosts?.length) {
-        for (let i = 0; i < this.feedPosts.length; i++) {
-          this.getPostAuthor(this.feedPosts[i]);
+  getPostAuthor(postAuthor: IPost, index: number): void {
+    this.tempUsername = '';
+    // this.feedUserID = null;
+    if (this.userProfileSub) {
+      this.userProfileSub.subscribe(userProfiles => {
+        let userProfile = userProfiles.find(profile => profile.id === postAuthor.author?.id);
+        this.feedUsernames[index][0].imageType = userProfile?.profilePictureContentType;
+        this.feedUsernames[index][0].imageURL = userProfile?.profilePicture;
+        if (userProfile?.user) {
+          this.feedUserID = userProfile.user?.id;
+          // Now that we have the user profile, get the username
+          this.userService.getUsers().subscribe(users => {
+            let user = users.find(user => user.id === userProfile?.user?.id);
+            if (user?.login) {
+              this.tempUsername = user.login;
+              this.feedUsernames[index][0].username = this.tempUsername;
+            }
+          });
         }
-      }
-    });
-  }
-
-  openFile(base64String: string, contentType: string | null | undefined): void {
-    return this.dataUtils.openFile(base64String, contentType);
+      });
+    }
   }
 
   trackId = (_index: number, item: IPost): number => this.postService.getPostIdentifier(item);
