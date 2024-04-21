37f17bcd00d989039ec3acd7368ea064afb88130
raphael1-w <himwong.raphael@gmail.com>
2024-04-21 03:50:19 +0100

    Revert "Fixing more merge conflicts"
    This reverts commit b5e5755e71cbe70f370733ee3704eb75e54226a5.

--- src/main/webapp/app/user-gallery/user-gallery.component.ts
+++ src/main/webapp/app/user-gallery/user-gallery.component.ts
@@ -51,9 +51,7 @@ export class UserGalleryComponent implements OnInit {
       this.userManagementService.find(this.account.login).subscribe({
         next: currentUser => {
           if (currentUser.id != null) {
-            // @ts-ignore
             this.allUserProfiles = this.userProfileService.getUserProfiles();
-            // @ts-ignore
             this.allUserProfiles.subscribe(userProfiles => {
               this.filteredProfile = userProfiles.filter(profile => profile.user?.id == currentUser.id);
               this.currentID = this.filteredProfile[0].id;
@@ -91,9 +89,7 @@ export class UserGalleryComponent implements OnInit {
         next: currentUser => {
           if (currentUser.id != null) {
             this.currentID = currentUser.id;
-            // @ts-ignore
             this.allUserProfiles = this.userProfileService.getUserProfiles();
-            // @ts-ignore
             this.allUserProfiles.subscribe(userProfiles => {
               this.filteredProfile = userProfiles.filter(profile => profile.user?.id == currentUser.id);
               this.currentProfile = this.filteredProfile[0];
