f82ea13a8e72916b4384383c02ecfc38065c1263
raphael1-w <himwong.raphael@gmail.com>
2024-04-21 03:50:10 +0100

    Revert "Revert "Fixing more merge conflicts""
    This reverts commit 2a554249f059447d97cc9ddd3eddd3f59996c573.

--- src/main/java/team/bham/domain/UserProfile.java
+++ src/main/java/team/bham/domain/UserProfile.java
@@ -564,13 +564,11 @@ public class UserProfile implements Serializable {
 
     public UserProfile addChatroom(Chatroom chatroom) {
         this.chatrooms.add(chatroom);
-        chatroom.getUserProfiles().add(this);
         return this;
     }
 
     public UserProfile removeChatroom(Chatroom chatroom) {
         this.chatrooms.remove(chatroom);
-        chatroom.getUserProfiles().remove(this);
         return this;
     }
 
